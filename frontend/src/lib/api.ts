49a085de8131d745395f72a0a6bff82b0cd52612
johnpauldaroy <cybercattribez@gmail.com>
2026-02-20 13:26:33 +0800

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://apicompliance.barbazampc.cloud/api',
-    withCredentials: true,
-    xsrfCookieName: 'XSRF-TOKEN',
-    xsrfHeaderName: 'X-XSRF-TOKEN',
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    },
+  baseURL: 'https://apicompliance.barbazampc.cloud',
+  withCredentials: true,
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
 });
 
 export default api;
-
